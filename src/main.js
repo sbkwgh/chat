94cdfaa489142aee67d504e47957d3153f0d6c0c
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2018-03-20 15:35:39 +0000

    Add index route

--- src/main.js
+++ src/main.js
@@ -4,13 +4,16 @@ import VueRouter from 'vue-router';
 import App from './App';
 
 //Routes
+import Index from './routes/index';
 import Conversation from './routes/conversation';
 
 Vue.config.productionTip = false;
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+	mode: 'history',
 	routes: [
+		{ path: '/', component: Index },
 		{ path: '/conversation/:id', component: Conversation }
 	]
 });
---
+++ src/routes/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class='index'>
+		<div class='index__message'>
+			<div classs='index__message__icon'></div>
+			<div class='index__message__text'>Select a conversation or click + to start a new one</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'index'
+	};
+</script>
+
+<style lang='scss' scoped>
+	@import '../assets/scss/variables';
+
+	.index {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
