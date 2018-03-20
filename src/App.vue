8707264ae67e02be8ab3f03fbe16b1b9193d1f7a
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2018-03-20 15:07:57 +0000

    Add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,42 @@
 <template>
 	<div id="app">
+		<side-panel></side-panel>
 	</div>
 </template>
 
 <script>
+	import SidePanel from './components/side-panel';
+
 export default {
 	name: 'app',
 	components: {
+		SidePanel
 	}
 };
 </script>
 
 <style lang='scss'>
+	@import './assets/scss/variables.scss';
+	@import './assets/scss/elements.scss';
+
+	@import url('https://fonts.googleapis.com/css?family=Fira+Sans:300,400,500');
+
+	* {
+		box-sizing: border-box;
+	}
+
+	html, body, #app {
+		color: $text-primary;
+		font-family: $font-family;
+		height: 100%;
+		margin: 0;
+		padding: 0;
+		width: 100%;
+	}
+
+	#app {
+		display: flex;
+		height: 100%;
+		width: 100%;
+	}
 </style>
---
+++ src/components/side-panel.vue
@@ -0,0 +1,54 @@
+<template>
+	<div class='side_panel'>
+		<div class='side_panel__header'>
+			Chat
+		</div>
+		<div class='side_panel__search'>
+			<input type='text' placeholder='Search conversations'>
+		</div>
+		<div class='side_panel__conversations'>
+			<side-panel-conversation v-for='conversation in conversations'></side-panel-conversation>
+		</div>
+	</div>
+</template>
+
+<script>
+	import SidePanelConversation from './side-panel-conversation';
+	export default {
+		name: 'side-panel',
+		components: {
+			SidePanelConversation
+		},
+		data () {
+			return {
+				conversations: (new Array(10)).fill(0)
+			};
+		}
+	};
+</script>
+
+<style lang='scss' scoped>
+	@import '../assets/scss/variables.scss';
+
+	.side_panel {
+		border-right: thin solid $gray-1;
+		height: 100%;
+		width: 17rem;
+
+		@at-root #{&}__header {
+			font-size: 2.5rem;
+			font-weight: 300;
+			padding: 0.5rem;
+			padding-top: 1rem;
+			text-align: center;
+		}
+		@at-root #{&}__search {
+			padding: 0.5rem;
+		}
+		@at-root #{&}__conversations {
+			border-top: thin solid $gray-1;
+			height: calc(100% - 7.125rem);
+			overflow-y: auto;
+		}
+	}
+</style>
