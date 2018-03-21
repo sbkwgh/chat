48cf12464dae9e2cea5adb3d986c74482a3f4a4d
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2018-03-21 22:09:07 +0000

    Change message

--- src/routes/index.vue
+++ src/routes/index.vue
@@ -2,7 +2,7 @@
 	<div class='index'>
 		<div class='index__message'>
 			<div classs='index__message__icon'></div>
-			<div class='index__message__text'>Select a conversation or click + to start a new one</div>
+			<div class='index__message__text'>Select a conversation or click 'New conversation' to start a new one</div>
 		</div>
 	</div>
 </template>
