34bcb9dcfe28ba7b38f21edca94297cff7243d20
sbkwgh <5166028+sbkwgh@users.noreply.github.com>
2018-04-02 19:50:20 +0100

    Add and use horizontal-slide transition

--- frontend/src/routes/login.vue
+++ frontend/src/routes/login.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class='login'>
-		<transition name='transition-fade' mode='out-in'>
+		<transition name='transition-horizontal-slide' mode='out-in'>
 			<form key='login' class='login__form' v-if='showLogin'>
 				<label>
 					<span>Username</span>
